15c99de4db4554125670dff662ebf4ca8a9ec66c
joshwashywash <joshwashywash@gmail.com>
2020-01-29 21:41:21 -0600

    Updates

---
+++ precache-manifest.5c2ad52235b7e820fd8cf039f1a2a214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774430697cd2a4874b09eef906cac8bc",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "357fc4c10c1d8b6a2565",
+    "url": "/pokedex/static/js/2.5d0a2b71.chunk.js"
+  },
+  {
+    "revision": "b5e3611fe954e5f2d0347d9ef16186af",
+    "url": "/pokedex/static/js/2.5d0a2b71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9beaef514337dedd69c6",
+    "url": "/pokedex/static/js/main.15f14ddb.chunk.js"
+  },
+  {
+    "revision": "c91897c97e6a66ab4a51",
+    "url": "/pokedex/static/js/runtime-main.a18cd847.js"
+  }
+]);
